c6b627aac3f80a971611c0417419579468e49834
AsiyeGokalp <107327598+AsiyeGokalp@users.noreply.github.com>
2022-10-27 06:13:56 +0200

    Update heartedPhotosView.js

--- src/view/heartedPhotosView.js
+++ src/view/heartedPhotosView.js
@@ -1,9 +1,23 @@
 'use strict';
-import { heartedDiv } from "./heartedView.js";
+
 const gallery = document.querySelector('.gallery');
 
 export async function createdHeartedImg() {
   gallery.innerHTML = '';
   const heartedPhotos = JSON.parse(localStorage.getItem('hearted'));
-  heartedDiv(heartedPhotos)
+
+  heartedPhotos.forEach((element) => {
+    const galleryImg = document.createElement('div');
+    galleryImg.classList.add('gallery-img');
+    galleryImg.innerHTML = `
+    
+            <div class="gallery-info">
+            <a id="heart" data-name="heart"><ion-icon name="heart"></ion-icon></a>
+            <a id="download" href='${element}'><ion-icon name="download-outline"></ion-icon></a>
+            </div>
+            <img src=${element}></img>
+
+            `;
+    gallery.appendChild(galleryImg);
+  });
 }
