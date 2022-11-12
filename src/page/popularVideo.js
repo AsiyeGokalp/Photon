b84acd16bd71573fbc4d2451efa92fcb4385f5ca
AsiyeGokalp <107327598+AsiyeGokalp@users.noreply.github.com>
2022-11-13 00:43:36 +0100

    Update popularVideo.js

--- src/page/popularVideo.js
+++ src/page/popularVideo.js
@@ -1,18 +1,18 @@
-// 'use strict';
+ 'use strict';
 
-// import { fetchApi } from './fetchApi.js';
-// import { generateVideo } from '../view/generateVideo.js';
+ import { fetchApi } from './fetchApi.js';
+ import { generateVideo } from '../view/generateVideo.js';
 
-// const gallery = document.querySelector('.gallery');
-// const more = document.querySelector('.more');
+ const gallery = document.querySelector('.gallery');
+ const more = document.querySelector('.more');
 
-// export async function popularVideos() {
-//   gallery.innerHTML = '';
+ export async function popularVideos() {
+   gallery.innerHTML = '';
 
-//   const data = await fetchApi(
-//     'https://api.pexels.com/videos/popular?per_page=225&page=1'
-//   );
+   const data = await fetchApi(
+     'https://api.pexels.com/videos/popular?per_page=225&page=1'
+   );
 
-//   generateVideo(data);
-//   more.classList.add('hide');
-// }
+   generateVideo(data);
+   more.classList.add('hide');
+ }
